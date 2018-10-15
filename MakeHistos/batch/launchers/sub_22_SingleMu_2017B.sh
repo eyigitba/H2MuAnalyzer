ad56251943ade6232cd68b248aa7844c7a255560
Xunwu Zuo <zuoxunwu@gmail.com>
2018-10-15 12:31:39 +0200

    removing things

--- MakeHistos/batch/launchers/sub_22_SingleMu_2017B.sh
+++
@@ -1,5 +0,0 @@
-
-run_dir="/afs/cern.ch/work/x/xzuo/h2mm_944/src/H2MuAnalyzer/MakeHistos"
-cd ${run_dir}
-eval `scramv1 runtime -sh`
-root -b -l -q '/afs/cern.ch/work/x/xzuo/h2mm_944/src/H2MuAnalyzer/MakeHistos/macros/MC_data_comparison.C("SingleMu_2017B", "/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/data_2017_and_mc_fall17/SingleMuon/SingleMu_2017B/180802_163835", "/afs/cern.ch/work/x/xzuo/public/H2Mu/2018/Histograms/WH_cat_2017_v4_v4/files", {"0000/tuple_52.root", "0000/tuple_53.root", "0000/tuple_54.root"}, "22", -1, 1000, 0.000000)'
--- MakeHistos/batch/launchers/sub_35_SingleMu_2017B.sh
+++
@@ -1,5 +0,0 @@
-
-run_dir="/afs/cern.ch/work/x/xzuo/h2mm_944/src/H2MuAnalyzer/MakeHistos"
-cd ${run_dir}
-eval `scramv1 runtime -sh`
-root -b -l -q '/afs/cern.ch/work/x/xzuo/h2mm_944/src/H2MuAnalyzer/MakeHistos/macros/MC_data_comparison.C("SingleMu_2017B", "/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/data_2017_and_mc_fall17/SingleMuon/SingleMu_2017B/180802_163835", "/afs/cern.ch/work/x/xzuo/public/H2Mu/2018/Histograms/WH_cat_2017_v4_v4/files", {"0000/tuple_8.root", "0000/tuple_80.root", "0000/tuple_81.root"}, "35", -1, 1000, 0.000000)'
