dafceba265b5befcaed9b9cca240e5e1316c37c7
Andrew Brinkerhoff <abrinke1@cern.ch>
2019-03-12 16:42:31 +0100

    Added net signal, background, and data histograms to StackPlot output

---
+++ DrawStackPlots/scripts/draw_stacks.sh
@@ -0,0 +1,9 @@
+
+
+macros/StackPlots.py  0  9
+macros/StackPlots.py 10 19
+macros/StackPlots.py 20 29
+macros/StackPlots.py 30 39
+macros/StackPlots.py 40 49
+macros/StackPlots.py 50 59
+macros/StackPlots.py 60 99
