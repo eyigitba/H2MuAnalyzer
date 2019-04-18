bf124ca8336d79ed9e05d4b52419ffd177b1c8c1
Xunwu Zuo <zuoxunwu@gmail.com>
2019-04-18 16:56:44 +0200

    temporary commit, will be overwritten

--- MakeHistos/batch/GenerateBatchScripts.py
+++ MakeHistos/batch/GenerateBatchScripts.py
@@ -37,15 +37,18 @@ if 'xzuo'     in os.getcwd(): USER = 'xzuo'
 
 #MACRO = 'macros/WH_lep.C'
 # MACRO = 'macros/ttH_3l.C'
-#MACRO = 'macros/MiniNTupliser.C'
-#MACRO = 'macros/lepMVA_efficiency.C'
-MACRO = 'macros/lepMVA_variables.C'
+MACRO = 'macros/MiniNTupliser.C'
+#MACRO = 'macros/MiniNTupliser_4l_cat.C'
+#MACRO = 'macros/lepMVA_SF_calc.C'
+#MACRO = 'macros/lepMVA_variables.C'
 
-LOC    = 'CERN'  ## Location of input files ('CERN', 'CERN_hiM', or 'UF')
+#LOC    = 'CERN'  ## Location of input files ('CERN', 'CERN_hiM', or 'UF')
 #LOC   = 'CERN_lepMVA_test_v2'  ## Location of input files ('CERN', 'CERN_hiM', or 'UF', or 'CERN_lepMVA_test_v1')
+LOC   = 'CERN_lepMVA_3l_test_v1_with_extra_samples'
 YEAR   = 2017    ## Dataset year (2016 or 2017)
 LUMI   = 41000   ## 36814 for 2016, 41000 for 2017
 ## Override default sample location from SampleDatabase.py (use IN_DIR = '' to keep default)
+IN_DIR = ''
 IN_DIR  = '/eos/cms/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/2017/94X_v2/2019_01_15_LepMVA_3l_test_v1'
 HADD_IN = True   ## Use pre-hadded root files (NTuple_*.root) instead of original files (tuple_*.root)
 
@@ -55,9 +58,11 @@ if USER == 'xzuo':     OUT_DIR = '/afs/cern.ch/work/x/xzuo/public/H2Mu/2018/Hist
 
 #LABEL  = 'WH_lep_AWB_2019_01_19_lepMVA_test_v1'
 # LABEL  = 'ttH_3l_AWB_2019_01_19_lepMVA_test_v1'
-LABEL = 'lepMVA_variables_v3_some_test'
-#LABEL = 'miniNtuple_WH_2017_v5'
-#LABEL   = 'WH_cat_2017_v4_v4' 
+#LABEL = 'lepMVA_variables_v3_some_test'
+#LABEL = 'lepMVA_ttH_3l_ele_v2_miniNtuple_dimu_sel_dimu_pt_v1'
+#LABEL = 'ZH_4l_ele_angle_test_v1'
+#LABEL = 'lepMVA_SF_v1'
+LABEL = 'WH_mu_med_ID_loose_iso_v1'
 
 NJOBS   =   -1  ## Maximum number of jobs to generate
 JOBSIZE =  100  ## Size of input NTuples in MB, per job (default 1000)
