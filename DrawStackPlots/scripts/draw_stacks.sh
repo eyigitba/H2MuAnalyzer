dccf79395c6224be01970feef08876216a2047dd
Andrew Brinkerhoff <abrinke1@cern.ch>
2019-05-21 16:43:51 +0200

    Add MC group histos to stack plot drawing

--- DrawStackPlots/scripts/draw_stacks.sh
+++ DrawStackPlots/scripts/draw_stacks.sh
@@ -6,4 +6,13 @@ macros/StackPlots.py 20 29
 macros/StackPlots.py 30 39
 macros/StackPlots.py 40 49
 macros/StackPlots.py 50 59
-macros/StackPlots.py 60 99
+macros/StackPlots.py 60 69
+macros/StackPlots.py 70 79
+macros/StackPlots.py 80 89
+macros/StackPlots.py 90 99
+macros/StackPlots.py 100 109
+macros/StackPlots.py 110 119
+macros/StackPlots.py 120 129
+macros/StackPlots.py 130 139
+macros/StackPlots.py 140 149
+macros/StackPlots.py 150 199
