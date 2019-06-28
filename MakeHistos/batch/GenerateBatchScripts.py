599c267a38b558bfae84294a3c213ff24157b87a
Andrew Brinkerhoff <abrinke1@cern.ch>
2019-06-28 17:05:32 +0200

    Add 2018 NTuples to SampleDatabase

--- MakeHistos/batch/GenerateBatchScripts.py
+++ MakeHistos/batch/GenerateBatchScripts.py
@@ -45,11 +45,9 @@ MACRO = 'macros/WH_lep.C'
 #MACRO = 'macros/lepMVA_efficiency.C'
 #MACRO = 'macros/lepMVA_variables.C'
 
-#LOC    = 'CERN'  ## Location of input files ('CERN', 'CERN_hiM', or 'UF')
-#LOC  = 'CERN_lepMVA_test_v2'  ## Location of input files ('CERN', 'CERN_hiM', or 'UF', or 'CERN_lepMVA_test_v1')
-LOC   = 'CERN_lepMVA_3l_test_v1'
-YEAR   = 2017    ## Dataset year (2016, 2017, or 2018)
-LUMI   = 41500   ## 36814 for 2016, 41500 for 2017, 59100 for 2018
+LOC  = 'CERN_3l'  ## Location of input files ('CERN', 'CERN_hiM', 'CERN_3l', or 'UF')
+YEAR = 2017       ## Dataset year (2016, 2017, or 2018)
+LUMI = 41500      ## 36814 for 2016, 41500 for 2017, 59100 for 2018
 ## Override default sample location from SampleDatabase.py (use IN_DIR = '' to keep default)
 #IN_DIR  = '/eos/cms/store/group/phys_higgs/HiggsExo/H2Mu/UF/ntuples/2017/94X_v2/2019_01_14_LepMVA_2l_hiM_test_v2'
 IN_DIR = ''
